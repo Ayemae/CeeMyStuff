ee9385e50d5ce675da597318316465f32c34c873
Ayemae <alyssaalecci@gmail.com>
2021-09-18 01:19:03 -0400

    Fixed homepage routing

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-include_once 'components/info-head.php';
+include 'components/info-head.php';
 // Include router class
 include('library/Route.php');
 
@@ -10,14 +10,19 @@ $page = getPage($request);
 
 // Add base route (startpage)
 Route::add('/',function(){
-    echo 'This is a homepage';
+    $admin_panel = false;
+    include 'components/info-head.php';
+    $page_title = $page['Page_Name'];
+    include_once 'components/header.php';
+    echo '<h1>'.$set['site_name'].'</h1>';
+    include_once 'components/footer.php';
 });
 
 // custom pages route!
 Route::add('/'.$page['Page_Name'],function(){
     global $set;global $db;global $page;
     $admin_panel = false;
-    include_once 'components/info-head.php';
+    include 'components/info-head.php';
     $page_title = $page['Page_Name'];
     include_once 'components/header.php';
     echo '<h1>'.$page['Page_Header'].'</h1>';
