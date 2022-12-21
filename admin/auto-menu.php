25acaaddb5f7a2349fd748246fef35311c03acc4
Ayemae <alyssaalecci@gmail.com>
2022-12-21 00:58:37 -0500

    Fixed deprecated routes

--- admin/auto-menu.php
+++ admin/auto-menu.php
@@ -31,7 +31,7 @@ $menu = getMenu();
             <div>
                 <?show($option['External_Link'] < 1 ? $option['Page_Name'] : $option['Ext_Link_Name'])?>
                 <?show($option['Img_Path'] ? '<img src="'.$option['Img_Path'].'" alt="">' : null)?>
-                <input type="hidden" name="option[<?show($option['Page_ID'])?>][link]" value="<?show($option['Outgoing_Link'])?>">
+                <input type="hidden" name="option[<?show($option['Page_ID'])?>][link]" value="<?show($option['Ext_Link'])?>">
                 <!-- link to edit? -->
             </div>
             <div>
@@ -56,4 +56,4 @@ $menu = getMenu();
 </main>
 
 <?php
-include '_components/footer.php';
+include '_components/admin-footer.php';
